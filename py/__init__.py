ec4f9424ede6aaf3c8dd9c615b90ed384872b6b7
科林_Cyril Pilgrim <onepigge1999@gmail.com>
2025-02-13 12:19:37 +0800

    Fix import error

--- py/__init__.py
+++ py/__init__.py
@@ -1,1 +1,6 @@
-# This is a placeholder file to create the 'py' directory
+from . import llm_loader
+from . import image_generation
+from . import image_understanding
+from . import model_loader
+from . import openai_compatible_loader
+from . import image_preprocessor
