9a36ff6b1d6338d37f6a9ea530e74c9e4e8bf9dd
科林_Cyril Pilgrim <onepigge1999@gmail.com>
2025-03-06 15:38:20 +0800

    update

--- py/__init__.py
+++ py/__init__.py
@@ -5,3 +5,4 @@ from . import model_loader
 from . import openai_compatible_loader
 from . import image_preprocessor
 from . import VideoFileUploader # 新增导入
+from . import douban_movie_photos # 新增豆瓣电影剧照节点导入
